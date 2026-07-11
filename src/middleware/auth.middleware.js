fb5399f6053e301267e89f016b59b02d26bb4b87
Anandx3505 <118062162+Anandx3505@users.noreply.github.com>
2026-07-11 15:09:47 +0530

    Add JWT verification middleware for user authentication

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,26 @@
+import { User } from "../models/user.model";
+import { ApiError } from "../utils/ApiError";
+import { asyncHandler } from "../utils/AsyncHandler";
+import jwt from 'jsonwebtoken';
+
+
+const verifyJWT = asyncHandler(async (req,res,next)=>{
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
+        if(!token){
+            throw new ApiError(401, "unauthoriesd request, tkn missing")
+        }
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+
+        if(!user){
+            throw new ApiError(401, "User not found , invalid token")
+        }
+
+        req.user = user
+        next()
+
+    } catch (error) {
+        throw new ApiError(401, error?.message || "invalid Token")   
+    }
+})
